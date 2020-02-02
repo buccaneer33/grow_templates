51a9b54bcbab09049d80317e528c80c640265d85
buccaneer33 <buccaneer33@inbox.ru>
2020-02-02 20:18:54 +0400

    [WIP]: add variables

--- template2/src/js/fonts.js
+++ template2/src/js/fonts.js
@@ -1,5 +1,3 @@
 'use strict';
 /*import '../../assets/fonts/OpenSans-Regular.ttf';
-import '../../assets/fonts/NoticiaText-Regular.ttf';
-import '../../assets/fonts/Raleway-Regular.ttf';
-import '../../assets/fonts/Raleway-Medium.ttf';*/
+*/
