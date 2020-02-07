adb99b618bee8b38c582b0db03e1b8b88a28ff30
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2020-02-07 15:17:32 +0400

    [WIP] add fonts and images

---
+++ template3/src/js/fonts.js
@@ -0,0 +1,18 @@
+'use strict';
+/*import '../../assets/fonts/Cabin-Medium.ttf';
+import '../../assets/fonts/Lora-Regular.ttf';*/
+// awesome
+import '../../assets/fonts/awesome/fa-brands-400.eot';
+import '../../assets/fonts/awesome/fa-brands-400.ttf';
+import '../../assets/fonts/awesome/fa-brands-400.woff';
+import '../../assets/fonts/awesome/fa-brands-400.woff2';
+
+import '../../assets/fonts/awesome/fa-regular-400.eot';
+import '../../assets/fonts/awesome/fa-regular-400.ttf';
+import '../../assets/fonts/awesome/fa-regular-400.woff';
+import '../../assets/fonts/awesome/fa-regular-400.woff2';
+
+import '../../assets/fonts/awesome/fa-solid-900.eot';
+import '../../assets/fonts/awesome/fa-solid-900.ttf';
+import '../../assets/fonts/awesome/fa-solid-900.woff';
+import '../../assets/fonts/awesome/fa-solid-900.woff2';
