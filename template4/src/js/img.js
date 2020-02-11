649e9a1cac66a79bc33e1d890de572233b49d88c
buccaneer33 <buccaneer33@inbox.ru>
2020-02-11 22:43:54 +0400

    [FINISHED]: temp4 start

---
+++ template4/src/js/img.js
@@ -0,0 +1,21 @@
+'use strict';
+import '../../assets/icons/favicon.ico';
+import '../../assets/icons/favicon.png';
+
+/*import '../../assets/img/background.jpg';
+import '../../assets/img/background-02.jpg';
+import '../../assets/img/background-03.jpg';
+import '../../assets/img/background-04.jpg';
+
+import '../../assets/img/01.jpg';
+import '../../assets/img/02.jpg';
+import '../../assets/img/03.jpg';
+import '../../assets/img/04.jpg';
+import '../../assets/img/05.jpg';
+import '../../assets/img/06.jpg';
+import '../../assets/img/07.jpg';
+import '../../assets/img/08.jpg';
+import '../../assets/img/09.jpg';
+import '../../assets/img/10.jpg';
+import '../../assets/img/11.jpg';
+import '../../assets/img/12.jpg';*/
